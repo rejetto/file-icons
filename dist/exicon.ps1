a648d8373c6a6e1ca859f0e724eaad7a3c6bbc2c
Massimo Melina <a@rejetto.com>
2024-06-29 09:37:22 +0200

    new config "use system icon"

---
+++ dist/exicon.ps1
@@ -0,0 +1,21 @@
+Add-Type -AssemblyName System.Drawing
+foreach ($file in $args) {
+  if (Test-Path $file) {
+    try {
+      $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($file)
+      $memoryStream = New-Object System.IO.MemoryStream
+      $icon.ToBitmap().Save($memoryStream, [System.Drawing.Imaging.ImageFormat]::Png)
+      $adsPath = "${file}_icon.png"
+      Write-Host "writing $adsPath"
+      $memoryStream.Position = 0  # Reset stream position
+      $fileStream = [System.IO.File]::Open($adsPath, [System.IO.FileMode]::OpenOrCreate, [System.IO.FileAccess]::Write)
+      $memoryStream.CopyTo($fileStream)
+      $fileStream.Close()
+      $memoryStream.Close()
+    } catch {
+      Write-Warning "Failed to process file: $file. Error: $_"
+    }
+  } else {
+    Write-Warning "File not found: $file"
+  }
+}
--- dist/plugin.js
+++ dist/plugin.js
@@ -1,6 +1,6 @@
-exports.version = 2.3
+exports.version = 3
 exports.description = "Customize file icons"
-exports.apiRequired = 8.1 // entryIcon
+exports.apiRequired = 8.891 // singleWorkerFromBatchWorker-returning
 exports.frontend_js = 'main.js'
 exports.repo = "rejetto/file-icons"
 
@@ -11,6 +11,7 @@ exports.configDialog = {
 const fileMask = '*.png|*.ico|*.jpg|*.jpeg|*.gif|*.svg'
 exports.config = {
     folders: { frontend: true, type: 'real_path', fileMask, label: "Icon for folders" },
+    system: { frontend: true, defaultValue: '', label: "Use system icon for", placeholder: 'Example: pdf|doc', helperText: "⚠️ Windows only" },
     icons: {
         frontend: true,
         label: "Icons for files by extension",
@@ -23,17 +24,50 @@ exports.config = {
     files: { frontend: true, type: 'real_path', fileMask, label: "Icon for other files" },
 }
 
-exports.init = api => ({
-    middleware: ctx => {
-        const {fileIcon} = ctx.query
-        if (!fileIcon) return
-        const {matches} = api.require('./misc')
-        const icon = fileIcon === '.' ? api.getConfig('folders')
-            : fileIcon === '*' ? api.getConfig('files')
-                : api.getConfig('icons')?.find(x => matches(fileIcon, x.ext))?.iconFile
-        if (!icon) return
-        const {serveFile} = api.require('./serveFile')
-        ctx.state.considerAsGui = true
-        return serveFile(ctx, icon)
+exports.init = api => {
+    const { matches, basename } = api.require('./misc')
+    const { runCmd } = api.require('./util-os')
+    const { readFile, unlink } = api.require('fs/promises')
+
+    const getIcon = api.require('./misc').singleWorkerFromBatchWorker(async jobs => {
+        const sources = jobs.flat()
+        await runCmd('powershell -ExecutionPolicy Bypass -File exicon.ps1', sources, { cwd: __dirname })
+        return Promise.all(sources.map(fn => {
+            fn += '_icon.png'
+            return readFile(fn).then(x => {
+                unlink(fn)
+                return x
+            }, api.log)
+        }))
+    }, { maxWait: 500 })
+
+    const cache = {}
+    const sampleFiles = {}
+
+    return {
+        async middleware(ctx) {
+            const { fileIcon } = ctx.query
+            if (!fileIcon) return
+            if (matches(fileIcon, api.getConfig('system'))) {
+                const sample = sampleFiles[fileIcon.toLowerCase()]
+                if (sample) {
+                    ctx.type = 'image/png'
+                    ctx.body = await (cache[fileIcon] ||= getIcon(sample))
+                    return
+                }
+            }
+            const icon = fileIcon === '.' ? api.getConfig('folders')
+                : fileIcon === '*' ? api.getConfig('files')
+                    : api.getConfig('icons')?.find(x => matches(fileIcon, x.ext))?.iconFile
+            if (!icon) return
+            const { serveFile } = api.require('./serveFile')
+            ctx.state.considerAsGui = true
+            return serveFile(ctx, icon)
+        },
+        onDirEntry({ node: { source } }) {
+            const ext = basename(source).split('.')[1].toLowerCase()
+            if (ext && !sampleFiles[ext] && matches(ext, api.getConfig('system'))) //TODO optimize
+                sampleFiles[ext] = source
+        }
     }
-})
+}
